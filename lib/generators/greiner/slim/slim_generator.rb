513f190296449eb54e42fd848b5dbc512b5f1479
mmgreiner <mmgreiner@bluewin.ch>
2025-06-22 12:33:36 +0200

    changed slimmer to slim

---
+++ lib/generators/greiner/slim/slim_generator.rb
@@ -0,0 +1,22 @@
+module Greiner
+  class SlimGenerator < Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
+
+    desc "Add slim-rails gem and configure Rails to use Slim"
+
+    def add_slim_gem
+      gem 'slim-rails', group: :development
+      run 'bundle install'
+    end
+
+    def configure_slim
+      # gsub_file 'config/application.rb', '# config.generators.template_engine = :erb', '  config.generators.template_engine = :slim'
+      lines = <<~SLIM.indent(4)
+        # use slim templating
+        config.generators.template_engine = :slim
+
+      SLIM
+      inject_into_file 'config/application.rb', lines + "\n", before: "  end"
+    end
+  end
+end
